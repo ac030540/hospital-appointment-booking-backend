5bbd8e7fc0d5718f2265690d3272ec4fd92018df
ac030540 <ac030540@gmail.com>
2019-10-21 21:53:54 +0530

    Fixed the nodemailer issues

--- controllers/appointmentBooking.js
+++ controllers/appointmentBooking.js
@@ -2,7 +2,9 @@ const nodemailer = require("./nodemailer")
 
 const appointmentHandler = (req, res,DB) => {
     const { doctorId, hospitalId } = req.params;
-	const {name, age, address, date, number} = req.body;
+    console.log('Entered appointemnt handler');
+  const {name, age, address, date, number, email} = req.body;
+  console.log(name, address, age, date, number, email);
 	DB('appointment').insert(
        { // {aid: DEFAULT,
         patient_name: name,
@@ -10,13 +12,18 @@ const appointmentHandler = (req, res,DB) => {
         patient_age: age,
         patient_number: number,
         a_date: date,
+        patient_email: email,
         a_hid: hospitalId, 
         a_did: doctorId}
         )
     .then( user_data => {
         if(user_data){
-            let composeMail = nodemailer.composeMail("Your Appointment has been booked",name);
-            res.json("success")
+            let message = nodemailer.composeMail(`Your Appointment has been booked successfully for date ${date}.`,name);
+            nodemailer.sendEmail("Appointment Confirmation", email, message)
+            .then(() => {
+              res.json("success");
+            })
+            .catch((err) => res.status(400).json(err));
         }
         else 
             res.status(400).json("Profile not found")
