dd26ee8250273547266daf867dd8d25014e9af2d
ac030540 <ac030540@gmail.com>
2019-10-21 13:50:09 +0530

    Added one one more function

--- controllers/doctors.js
+++ controllers/doctors.js
@@ -1,4 +1,16 @@
-const doctorDetailsHandler = (req, res,DB) => {
+const doctorDetailsByIdHandler = (req, res,DB) => {
+	const {id} = req.params;
+	DB.select('*').from('users').where({id:id})
+    .then( user => {
+	  	if(user.length)
+	  		res.json(user[0])
+	  	else 
+	  		res.status(400).json("Profile not found")
+	})
+    .catch( err => res.status(400).json("Unable to load profile"))
+}
+
+const allDoctorDetailsHandler = (req, res,DB) => {
 	const {id} = req.params;
 	DB.select('*').from('users').where({id:id})
     .then( user => {
@@ -11,5 +23,6 @@ const doctorDetailsHandler = (req, res,DB) => {
 }
 
 module.exports = {
-	doctorDetailsHandler : doctorDetailsHandler
+  doctorDetailsByIdHandler : doctorDetailsByIdHandler,
+  allDoctorDetailsHandler: allDoctorDetailsHandler
 }
--- server.js
+++ server.js
@@ -22,7 +22,8 @@ const appointmentBooking = require('./controllers/appointmentBooking');
 
 app.get( '/hospitals', (req, res) => { hospitals.allHospitalDetailsHandler(req, res, DB)});
 app.get( '/hospitalPage/:hospitalId', (req, res) => { hospitalDetails.hospitalDetailsByIdHandler(req, res, DB)});
-app.get( '/doctors/:hospitalId', (req, res) => { doctors.doctorDetailsHandler(req, res, DB)});
+app.get( '/doctors/:hospitalId', (req, res) => { doctors.allDoctorDetailsHandler(req, res, DB)});
+app.get( '/doctors/:hospitalId/:doctorId', (req, res) => { doctors.doctorDetailsByIdHandler(req, res, DB)});
 app.post( '/booking-appointment/:hospitalId/:doctorId', (req, res) => { appointmentBooking.appointmentHandler(req, res, DB)});
 
 const PORT = process.env.PORT || 4000;
